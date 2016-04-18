1a37cb36cd9cb59b8fbd6f50c19bdaaf1edd6d0a
Jason Williams <jaswilli@uchicago.edu>
2016-04-18 17:05:28 -0500

    Remove reference response bodies

--- service/utils.py
+++ service/utils.py
@@ -22,24 +22,6 @@ def token_introspect(token):
                                headers=dict(Authorization=basic_auth_header()),
                                data=dict(token=token))
 
-    # response body:
-    # {
-    #     "active": true,
-    #     "scope": "urn:globus:auth:scope:service.example.com:all",
-    #     "client_id": "client.example.com",
-    #     "sub": "2982f207-04c0-11e5-ac60-22000b92c6ec",
-    #     "username": "user1@example.com",
-    #     "aud": "server.example.com",
-    #     "iss": "https://auth.globus.org/",
-    #     "exp": 1419356238,
-    #     "iat": 1419350238,
-    #     "nbf": 1419350238,
-    #     “identities_set”: [“2982f207-04c0-11e5-ac60-22000b92c6ec”,
-    #     ”3982f207-04c0-11e5-ac60-22000b92c6ed”]
-    #     “name”: “Joe User”,
-    #     “email”: “user1@example.dom”
-    # }
-
     return token_data.json()
 
 
@@ -57,25 +39,5 @@ def get_dependent_tokens(token):
     tokens = requests.post(url,
                            headers=dict(Authorization=basic_auth_header()),
                            data=data)
-
-    # response body:
-    # [
-    #     {
-    #         "access_token": "r5qwkEz0lWJdpdknlDBmndC2G7wpTSOk...",
-    #         "resource_server": "auth.globus.org",
-    #         "scope": "urn:globus:auth:scope:auth.globus.org:view_identities",
-    #         "expires_in": 3600,
-    #         "refresh_token": "kUKDtLe_xDA4Qxd-ZI-rcFqrBlJj7zXx",
-    #         "token_type": "bearer"
-    #     },
-    #     {
-    #         "access_token": "7ZdPhhvija1MUDw6koBYgAAGnbrU79qF...",
-    #         "resource_server": "groups.api.globus.org",
-    #         "scope": "urn:globus:auth:scope:groups.api.globus.org:[...]",
-    #         "expires_in": 3600,
-    #         "refresh_token": "dPVJBKAUs0x8UW4zhgQWv6snDmo2X72E",
-    #         "token_type": "bearer"
-    #     }
-    # ]
 
     return tokens.json()
