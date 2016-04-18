b126ffe83b5bb92b0064e17396230e0e6f3f8214
Steve Tuecke <tuecke@users.noreply.github.com>
2016-04-18 16:24:42 -0500

    Changes for exercises (#40)
    * Changes for exercises
    
    * Adding spaces to fix lint

--- portal/__init__.py
+++ portal/__init__.py
@@ -16,4 +16,14 @@ database = Database(app)
 with open(app.config['DATASETS']) as f:
     datasets = json.load(f)
 
+# Full working portal
 import portal.views
+# import portal.views_graph
+
+# Exercise 1
+# import portal.views_ex1
+# import portal.views_graph_ex1
+
+# Exercise 2
+# import portal.views
+# import portal.views_graph_service_ex2
