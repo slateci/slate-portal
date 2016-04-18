6c4208387d5060d4f5e989cd91a27ec8f8922fb2
Jason Williams <jaswilli@uchicago.edu>
2016-04-18 17:19:20 -0500

    Resource server

--- service/decorators.py
+++ service/decorators.py
@@ -1,9 +1,12 @@
 from flask import g, jsonify, request
 from functools import wraps
 
+import requests
+
 from service import app
-from service.errors import UnauthorizedError, ForbiddenError
-from service.utils import token_introspect, get_token
+from service.errors import (BadRequestError, UnauthorizedError, ForbiddenError,
+                            InternalServerError)
+from service.utils import basic_auth_header, get_token
 
 
 def authenticated(fn):
@@ -13,18 +16,46 @@ def authenticated(fn):
         if 'Authorization' not in request.headers:
             raise UnauthorizedError()
 
+        # Get the access token from the request
         token = get_token(request.headers['Authorization'])
-        # do token introspection
-        # if request.headers['Authorization'] != 'Bearer good':
-        token_meta = token_introspect(token)
+        auth_header = basic_auth_header()
+        ga_token_url = app.config['GA_INTROSPECT_URI']
 
-        if token_meta.get('active', False) is False:
+        # Call /token/introspect
+        token_meta = requests.post(ga_token_url,
+                                   headers=dict(Authorization=auth_header),
+                                   data=dict(token=token))
+
+        if not token_is_valid(token_meta.json()):
             raise ForbiddenError()
 
+        # Token has passed verification so we attach it to the
+        # request global object and proceed
         g.req_token = token
 
         return fn(*args, **kwargs)
     return decorated_function
+
+
+def token_is_valid(data):
+    return (data.get('active') and
+            'GlobusWorld Resource Server' in data.get('aud', []))
+
+
+@app.errorhandler(BadRequestError)
+def handle_badrequest_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+
+    return response
+
+
+@app.errorhandler(InternalServerError)
+def handle_internalserver_error(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+
+    return response
 
 
 @app.errorhandler(UnauthorizedError)
--- service/views.py
+++ service/views.py
@@ -1,19 +1,178 @@
-from flask import g, request
+from flask import g, jsonify, request
 
-from service import app
+from globus_sdk import (TransferClient, TransferAPIError,
+                        DeleteData)
+
+import requests
+
+from service import app, datasets
 from service.decorators import authenticated
+from service.errors import BadRequestError, InternalServerError
+from service.processing import render_graphs
 from service.utils import get_dependent_tokens
 
 
 @app.route('/api/doit', methods=['POST'])
 @authenticated
 def doit():
-    get_dependent_tokens(g.req_token)
+    """
+    Add code here to:
 
-    return request.headers.get('Authorization', '')
+    - Read the year and the IDs of the datasets the user wants
+    - Instantiate a Transfer client as the identity of the portal
+    - `GET` the CSVs for the selected datasets via HTTPS server as the
+      identity of the portal
+    - Generate a graph SVG file for the precipitation (`PRCP`) and
+      temperature (`TMIN`/`TMAX`) for the selected year and datasets
+    - `PUT` the generated graphs onto the predefined share endpoint as
+      the identity of the portal
+    - Display a confirmation
+    """
+
+    dependent_tokens = get_dependent_tokens(g.req_token)
+
+    transfer_scope = 'urn:globus:auth:scope:transfer.api.globus.org:all'
+    http_scope = 'urn:globus:auth:scope:tutorial-https-endpoint.globus.org:all'
+
+    try:
+        transfer_token = next(token['access_token']
+                              for token in dependent_tokens
+                              if token['scope'] == transfer_scope)
+    except StopIteration:
+        raise InternalServerError(message='Problem with dependent token grant')
+
+    try:
+        http_token = next(token['access_token']
+                          for token in dependent_tokens
+                          if token['scope'] == http_scope)
+    except StopIteration:
+        raise InternalServerError(message='Problem with dependent token grant')
+
+    selected_ids = request.form.getlist('datasets')
+    selected_year = request.form.get('year')
+    user_identity_id = request.form.get('user_identity_id')
+    user_identity_name = request.form.get('user_identity_name')
+
+    selected_datasets = [dataset for dataset in datasets
+                         if dataset['id'] in selected_ids]
+
+    if not (selected_datasets and selected_year):
+        raise BadRequestError()
+
+    transfer = TransferClient(token=transfer_token)
+
+    source_ep = app.config['DATASET_ENDPOINT_ID']
+    source_info = transfer.get_endpoint(source_ep)
+    source_https = source_info['https_server']
+    source_base = app.config['DATASET_ENDPOINT_BASE']
+    source_token = http_token
+
+    dest_ep = app.config['GRAPH_ENDPOINT_ID']
+    dest_info = transfer.get_endpoint(dest_ep)
+    dest_https = dest_info['https_server']
+    dest_base = app.config['GRAPH_ENDPOINT_BASE']
+    dest_path = '%sGraphs for %s/' % (dest_base, user_identity_name)
+    dest_token = http_token
+
+    if not (source_https and dest_https):
+        raise InternalServerError(message='Endpoints must be HTTPS servers')
+
+    svgs = {}
+
+    for dataset in selected_datasets:
+        source_path = dataset['path']
+        response = requests.get('%s%s%s/%s.csv' % (source_https, source_base,
+                                                   source_path, selected_year),
+                                headers=dict(
+                                    Authorization='Bearer ' + source_token),
+                                allow_redirects=False)
+        response.raise_for_status()
+        svgs.update(render_graphs(
+            csv_data=response.iter_lines(decode_unicode=True),
+            append_titles=' from %s for %s' % (dataset['name'], selected_year),
+        ))
+
+    transfer.endpoint_autoactivate(dest_ep)
+
+    try:
+        transfer.operation_mkdir(dest_ep, dest_path)
+    except TransferAPIError as error:
+        if 'MkdirFailed.Exists' not in error.code:
+            raise
+
+    try:
+        transfer.add_endpoint_acl_rule(
+            dest_ep,
+            dict(principal=user_identity_id,
+                 principal_type='identity', path=dest_path, permissions='r'),
+        )
+    except TransferAPIError as error:
+        if error.code != 'Exists':
+            raise
+
+    for filename, svg in svgs.items():
+        # n.b. The HTTPS Server will overwrite existing files that you PUT.
+
+        requests.put('%s%s%s.svg' % (dest_https, dest_path, filename),
+                     data=svg,
+                     headers=dict(Authorization='Bearer ' + dest_token),
+                     allow_redirects=False).raise_for_status()
+
+    results = {
+        'dest_ep': dest_ep,
+        'dest_path': dest_path,
+        'dest_name': dest_info['display_name'],
+        'graph_count': len(svgs) or 0
+    }
+
+    return jsonify(results)
 
 
 @app.route('/api/cleanup', methods=['POST'])
 @authenticated
 def cleanup():
-    return request.headers.get('Authorization', '')
+    """
+    Add code here to:
+
+    - figure out the logged-in user's graph directory
+    - find the logged-in user's ACL for the graph directory
+    - delete both the directory and the associated ACL
+    """
+    user_identity_name = request.form.get('user_identity_name')
+
+    dependent_tokens = get_dependent_tokens(g.req_token)
+
+    transfer_scope = 'urn:globus:auth:scope:transfer.api.globus.org:all'
+
+    try:
+        transfer_token = next(token['access_token']
+                              for token in dependent_tokens
+                              if token['scope'] == transfer_scope)
+    except StopIteration:
+        raise InternalServerError(message='Problem with dependent token grant')
+
+    dest_ep = app.config['GRAPH_ENDPOINT_ID']
+    dest_base = app.config['GRAPH_ENDPOINT_BASE']
+    dest_path = '%sGraphs for %s/' % (dest_base, user_identity_name)
+
+    transfer = TransferClient(token=transfer_token)
+
+    try:
+        acl = next(acl for acl in transfer.endpoint_acl_list(dest_ep)
+                   if dest_path == acl['path'])
+    except StopIteration:
+        pass
+    else:
+        transfer.delete_endpoint_acl_rule(dest_ep, acl['id'])
+
+    delete_request = DeleteData(transfer_client=transfer, endpoint=dest_ep,
+                                label="Delete Graphs from the Service Demo",
+                                recursive=True)
+    delete_request.add_item(dest_path)
+
+    try:
+        task = transfer.submit_delete(delete_request)
+    except TransferAPIError as ex:
+        raise InternalServerError(message=ex.message)
+    else:
+        return jsonify(dict(task_id=task['task_id']))
