31c11f8b1180d08f5c7226c005550c6e973c55b3
Jaro <jeremyvan614@gmail.com>
2019-03-08 10:30:15 -0600

    adding misaka to app

--- portal/__init__.py
+++ portal/__init__.py
@@ -2,19 +2,14 @@ from flask import Flask
 import json
 
 from portal.database import Database
-# from flask_misaka import markdown
-# from flask_misaka import Misaka
+from flask_misaka import markdown
+from flask_misaka import Misaka
 
 __author__ = 'Jeremy Van'
 
-# md = Misaka()
+md = Misaka()
 app = Flask(__name__)
 app.config.from_pyfile('portal.conf')
-# md.__init__(app, tables=True)
-
-# database = Database(app)
-#
-# with open(app.config['DATASETS']) as f:
-#     datasets = json.load(f)
+md.__init__(app, tables=True)
 
 import portal.views
