b7fd176685bbb98fce98605ea1c65bf68bc8f104
Jaro <jeremyvan614@gmail.com>
2019-04-29 11:44:38 -0500

    update init py file

--- portal/__init__.py
+++ portal/__init__.py
@@ -7,20 +7,16 @@ from flask_misaka import markdown
 from flask_misaka import Misaka
 import logging.handlers
 import logging
-from flask_cors import CORS
 
 __author__ = 'Jeremy Van'
 # set up Flask App
 app = Flask(__name__)
 app.config.from_pyfile('portal.conf')
 app.url_map.strict_slashes = False
-# set up CORS
-CORS(app)
-# cors = CORS(application, resources={r'/*': {"origins": '*'}})
 
 # set up Markdown Rendering
 md = Misaka()
-md.__init__(app, tables=True, autolink=True, fenced_code=True, smartypants=True, quote=True, math=True, math_explicit=True, gfm=True)
+md.__init__(app, tables=True, autolink=True, fenced_code=True, smartypants=True, quote=True, math=True, math_explicit=True)
 
 # set up logging
 handler = logging.handlers.RotatingFileHandler(
