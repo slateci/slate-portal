a477d7fbdc77e49acd476db19228553864807802
Jeremy Van <jeremyvan614@gmail.com>
2020-07-21 01:09:32 -0500

    set user id session in decorator

--- portal/decorators.py
+++ portal/decorators.py
@@ -1,6 +1,6 @@
 from flask import redirect, request, session, url_for
 from functools import wraps
-
+from connect_api import get_user_id
 
 def authenticated(fn):
     """Mark a route as requiring authentication."""
@@ -17,7 +17,11 @@ def authenticated(fn):
             return redirect(url_for('create_profile', next=request.url))
         
         if (not session.get('user_id') and request.path != '/profile/new'):
-            return redirect(url_for('create_profile', next=request.url))
+            try:
+                user_id = get_user_id(session)
+                session['user_id'] = user_id
+            except:
+                return redirect(url_for('create_profile', next=request.url))
 
         return fn(*args, **kwargs)
     return decorated_function
