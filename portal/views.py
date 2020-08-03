f75d5d6dd0bcab203fe4e7bab7c783fc91bb0426
Jeremy Van <jeremyvan614@gmail.com>
2020-08-03 15:27:15 -0500

    make public groups async, updated route name from underscore to dash

--- portal/views.py
+++ portal/views.py
@@ -303,56 +303,6 @@ def cli_access():
         return render_template('cli_access.html', access_token=access_token, slate_api_endpoint=slate_api_endpoint)
 
 
-@app.route('/public_groups', methods=['GET', 'POST'])
-@authenticated
-def list_public_groups():
-    if request.method == 'GET':
-        access_token = get_user_access_token(session)
-        query = {'token': access_token}
-
-        s = requests.get(
-            slate_api_endpoint + '/v1alpha3/groups', params=query)
-
-        s_info = s.json()
-        group_list = s_info['items']
-
-        return render_template('groups_public.html', group_list=group_list)
-
-
-@app.route('/public_groups_ajax', methods=['GET'])
-def public_groups_ajax():
-    public_groups = list_public_groups_request()
-    return jsonify(public_groups)
-
-
-@app.route('/public_groups/<name>', methods=['GET', 'POST'])
-@authenticated
-def view_public_group(name):
-    access_token = get_user_access_token(session)
-    query = {'token': access_token}
-    if request.method == 'GET':
-
-        group_info_query = '/v1alpha3/groups/' + name + '?token=' +query['token']
-        group_clusters_query = '/v1alpha3/groups/' + name + '/clusters?token=' +query['token']
-        # Set up multiplex JSON
-        multiplexJson = {group_info_query: {"method":"GET"},
-                            group_clusters_query: {"method":"GET"}}
-        # POST request for multiplex return
-        multiplex = requests.post(
-            slate_api_endpoint + '/v1alpha3/multiplex', params=query, json=multiplexJson)
-        multiplex = multiplex.json()
-
-        # Parse post return for apps, clusters, and pub groups
-        try:
-            group_info_json = json.loads(multiplex[group_info_query]['body'])
-            group_clusters_json = json.loads(multiplex[group_clusters_query]['body'])
-            group_clusters_json = group_clusters_json['items']
-        except:
-            return render_template('404.html')
-
-        return render_template('groups_public_profile.html', group_info=group_info_json, group_clusters=group_clusters_json, name=name)
-
-
 @app.route('/mailgun/<group_name>/<user_name>/<user_email>', methods=['GET', 'POST'])
 @authenticated
 def mailgun(group_name, user_name, user_email):
--- portal/views/views_groups.py
+++ portal/views/views_groups.py
@@ -33,3 +33,43 @@ def view_user_groups():
         return jsonify(user_groups)
 
 
+@app.route('/public-groups', methods=['GET', 'POST'])
+@authenticated
+def list_public_groups():
+    if request.method == 'GET':
+        return render_template('groups_public.html')
+
+
+@app.route('/public-groups-xhr', methods=['GET'])
+def public_groups_ajax():
+    public_groups = list_public_groups_request()
+    return jsonify(public_groups)
+
+
+@app.route('/public-groups/<name>', methods=['GET', 'POST'])
+@authenticated
+def view_public_group(name):
+    access_token = get_user_access_token(session)
+    query = {'token': access_token}
+    if request.method == 'GET':
+
+        group_info_query = '/v1alpha3/groups/' + name + '?token=' +query['token']
+        group_clusters_query = '/v1alpha3/groups/' + name + '/clusters?token=' +query['token']
+        # Set up multiplex JSON
+        multiplexJson = {group_info_query: {"method":"GET"},
+                            group_clusters_query: {"method":"GET"}}
+        # POST request for multiplex return
+        multiplex = requests.post(
+            slate_api_endpoint + '/v1alpha3/multiplex', params=query, json=multiplexJson)
+        multiplex = multiplex.json()
+
+        # Parse post return for apps, clusters, and pub groups
+        try:
+            group_info_json = json.loads(multiplex[group_info_query]['body'])
+            group_clusters_json = json.loads(multiplex[group_clusters_query]['body'])
+            group_clusters_json = group_clusters_json['items']
+        except:
+            return render_template('404.html')
+
+        return render_template('groups_public_profile.html', group_info=group_info_json, group_clusters=group_clusters_json, name=name)
+
