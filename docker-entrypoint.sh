0e8e7897007db2b19fe3d5633e72c19ad0f46750
Adam H Griffith <adam.h.griffith@utah.edu>
2022-02-25 14:16:27 -0700

    Bringing portal/instance back into version control while ignoring its' contents.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,16 +3,6 @@
 # Enable strict mode:
 set -euo pipefail
 
-# Create local user:
-cat > /slate_portal_user <<EOF
-User_A95B266B-2327-4363-BEE0-A61DCF192DA6
-WebPortal
-slate@slateci.io
-555-5555
-SLATE
-5B121807-7D5D-407A-8E22-5F001EF594D4
-EOF
-
 # Run portal:
 cd /etc/slate
 source venv/bin/activate
