bd066fa3c98eb620554ff496563bb8ea952c8d55
Adam H Griffith <adam.h.griffith@utah.edu>
2023-01-06 13:37:53 -0700

    Not logging anything for /healthz under not_found().

--- portal/views/views_error_handling.py
+++ portal/views/views_error_handling.py
@@ -27,8 +27,7 @@ def errorpage(message):
 def not_found(e):
     if request.path.startswith('/healthz'):
         # K8s health checks are done against <pod_ipv4:port>, not <SERVER_NAME:port>.
-        # As a work-around we create a custom log.
-        app.logger.info("IGNORE: K8s health check. URL: {}".format(request.path))
+        # As a work-around we do not create an error log and return the template.
         return render_template('healthz.html')
     else:
         app.logger.error("{}. URL: {}".format(e, request.path))
